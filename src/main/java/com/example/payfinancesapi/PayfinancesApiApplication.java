36039536389bdac5095298db5c7b680cb8b06c17
ara898 <ara-j98@hotmail.com>
2025-09-20 19:07:53 -0300

    initial commit in payfinances

---
+++ src/main/java/com/example/payfinancesapi/PayfinancesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.payfinancesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayfinancesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayfinancesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/payfinancesapi/PayfinancesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payfinancesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayfinancesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
